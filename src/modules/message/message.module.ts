54af75e885284e79ec64f49f97e3bb033133aaa0
Berkant BEGDILILI <59812010+berkantbegdilili@users.noreply.github.com>
2022-02-18 17:39:58 +0300

    renamed from message.decarator.ts

--- src/modules/message/message.module.ts
+++ src/modules/message/message.module.ts
@@ -1,7 +1,7 @@
 import { DynamicModule, FactoryProvider, Module } from '@nestjs/common';
 import { MessageService } from '@modules/message/message.service';
 import { MessageOptions } from '@modules/message/interfaces/message-options.interface';
-import { MESSAGE_SERVICE } from '@modules/message/message.decarator';
+import { MESSAGE_SERVICE } from '@modules/message/message.decorator';
 
 @Module({})
 export class MessageModule {
