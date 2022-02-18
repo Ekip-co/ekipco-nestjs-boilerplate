a4073d08bf78f2111318b6beae74219f72e028c8
Berkant BEGDILILI <59812010+berkantbegdilili@users.noreply.github.com>
2022-02-18 17:33:58 +0300

    oauth token service have been added.

---
+++ src/modules/firebase/oauth-token.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@nestjs/common';
+import { FirebaseAdminService } from '@modules/firebase/firebase-admin.service';
+
+@Injectable()
+export class OAuthTokenService {
+    constructor(private fbAdmin: FirebaseAdminService) {}
+
+    getOAuthToken() {
+        const collectionPath = this.fbAdmin.getZohoTokenCollectionPath();
+        const documentPath = this.fbAdmin.getZohoTokenDocumentPath();
+        const fieldName = this.fbAdmin.getZohoTokenFieldName();
+
+        return this.fbAdmin
+            .getFirestore()
+            .collection(collectionPath)
+            .doc(documentPath)
+            .get()
+            .then((result) => result.data()[fieldName])
+            .catch((err) => {
+                throw err;
+            });
+    }
+
+    updateOAuthToken(token: string) {
+        const collectionPath = this.fbAdmin.getZohoTokenCollectionPath();
+        const documentPath = this.fbAdmin.getZohoTokenDocumentPath();
+
+        return this.fbAdmin
+            .getFirestore()
+            .collection(collectionPath)
+            .doc(documentPath)
+            .set({ token }, { merge: true });
+    }
+}
