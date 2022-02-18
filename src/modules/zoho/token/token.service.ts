5811d036108285dbbc84832f56784b1abd722775
Berkant BEGDILILI <59812010+berkantbegdilili@users.noreply.github.com>
2022-02-18 17:34:37 +0300

    zoho token module, controller and service have been added.

---
+++ src/modules/zoho/token/token.service.ts
@@ -0,0 +1,71 @@
+import { Inject, Injectable } from '@nestjs/common';
+import zohoConfig from '@config/zoho.config';
+import { ConfigType } from '@nestjs/config';
+import { HttpService } from '@nestjs/axios';
+import { readFileSync } from 'fs';
+import { CrmStatusCode } from '@modules/zoho/crm-status-code.interface';
+import { lastValueFrom, Observable } from 'rxjs';
+import { AxiosResponse } from 'axios';
+import { EkipException, NoResponseReceivedZohoException } from '@exceptions';
+
+@Injectable()
+export class ZohoTokenService {
+    private readonly statusCodes: CrmStatusCode[];
+
+    constructor(
+        @Inject(zohoConfig.KEY) private zohoCfg: ConfigType<typeof zohoConfig>,
+        private httpService: HttpService,
+    ) {
+        const statusCodesPath =
+            __dirname + '/../../../resources/crm-function-status-codes.json';
+        const statusCodesBuffers = readFileSync(statusCodesPath, {
+            encoding: 'utf8',
+        });
+        this.statusCodes = JSON.parse(statusCodesBuffers);
+    }
+
+    getNewAccessToken() {
+        const observable = this.executeAxiosInstance();
+        return lastValueFrom(observable)
+            .then((result: AxiosResponse<any>) => result.data)
+            .catch((err) => this.errorHandling(err));
+    }
+
+    private executeAxiosInstance(): Observable<AxiosResponse<any>> {
+        const url = this.zohoCfg.tokenURL;
+
+        const options = {
+            params: {
+                refresh_token: this.zohoCfg.refreshToken,
+                client_id: this.zohoCfg.clientId,
+                client_secret: this.zohoCfg.clientSecret,
+                code: this.zohoCfg.code,
+                grant_type: 'refresh_token',
+            },
+        };
+
+        return this.httpService.post(url, null, options);
+    }
+
+    private errorHandling(err: any) {
+        if (err.response) {
+            // The request was made and the server responded with a status code
+            // that falls out of the range of 2xx
+            const crmStatusCode = this.statusCodes.find(
+                (e: CrmStatusCode) => e.code === err.response.data.code,
+            );
+            const statusCode = crmStatusCode ? crmStatusCode.statusCode : 500;
+
+            throw new EkipException(
+                err.response.data.message,
+                statusCode || err.response.status,
+            );
+        } else if (err.request) {
+            // The request was made but no response was received from Zoho
+            throw new NoResponseReceivedZohoException(err);
+        } else {
+            // Something happened in setting up the request that triggered an Error
+            throw err;
+        }
+    }
+}
