a5cce6743fc37cfb0ae98297dd04fbae54c0c8e8
Berkant BEGDILILI <59812010+berkantbegdilili@users.noreply.github.com>
2022-02-18 17:42:21 +0300

    oauth token service have been added.

--- src/modules/firebase/firebase.module.ts
+++ src/modules/firebase/firebase.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { FirebaseAdminService } from './firebase-admin.service';
+import { OAuthTokenService } from '@modules/firebase/oauth-token.service';
 
 @Module({
-    providers: [FirebaseAdminService],
-    exports: [FirebaseAdminService],
+    providers: [FirebaseAdminService, OAuthTokenService],
+    exports: [FirebaseAdminService, OAuthTokenService],
 })
 export class FirebaseModule {}
