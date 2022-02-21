ff5c1d5f7a785384bfdba91ec485d3406e10fb9a
Berkant BEGDILILI <59812010+berkantbegdilili@users.noreply.github.com>
2022-02-21 10:40:40 +0300

    transform interceptor bug fixed

--- src/common/interceptors/transform.interceptor.ts
+++ src/common/interceptors/transform.interceptor.ts
@@ -19,6 +19,6 @@ export class TransformInterceptor<T>
         context: ExecutionContext,
         next: CallHandler,
     ): Observable<Response<T>> {
-        return next.handle().pipe(map((data) => ({ ...data, error: false })));
+        return next.handle().pipe(map((data) => ({ data, error: false })));
     }
 }
