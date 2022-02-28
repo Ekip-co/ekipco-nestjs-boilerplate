463777bb84b8883b96b5cb9656cde695d42db47d
Berkant BEGDILILI <59812010+berkantbegdilili@users.noreply.github.com>
2022-02-28 13:41:38 +0300

    Files APIs integration have been added and bugs fixed.

--- src/modules/zoho/core/crm/crm.service.ts
+++ src/modules/zoho/core/crm/crm.service.ts
@@ -5,15 +5,21 @@ import zohoConfig from '@config/zoho.config';
 import { ConfigType } from '@nestjs/config';
 import { lastValueFrom } from 'rxjs';
 import { AxiosResponse } from 'axios';
-import { NotFoundException } from '@exceptions';
+import { BadRequestException, NotFoundException } from '@exceptions';
 import { isDefined } from 'class-validator';
 import { OAuthTokenService } from '@modules/firebase/services/oauth-token.service';
-import { ZohoCrmOptions } from '@modules/zoho/core/crm/interfaces/crm-options.interface';
+import {
+    FileType,
+    ZohoCrmOptions,
+} from '@modules/zoho/core/crm/interfaces/crm-options.interface';
 import { File } from '@modules/zoho/core/crm/interfaces/file.interface';
 import { RecordApiResponse } from '@modules/zoho/core/crm/entities/record-api-response.entity';
 import { ZohoCoreService } from '../core.service';
+import { CoqlApiResponse } from '@modules/zoho/core/crm/entities/coql-api-response.entity';
+import { BadRequestExceptionType } from '@enums';
+import { FilesApiResponse } from '@modules/zoho/core/crm/entities/files-api-response.entity';
 
-type ZohoHttpMethod = 'POST' | 'PUT' | 'DELETE';
+type ZohoHttpMethod = 'GET' | 'POST' | 'PUT' | 'DELETE';
 
 @Injectable()
 export class ZohoCrmService extends ZohoCoreService {
@@ -29,6 +35,139 @@ export class ZohoCrmService extends ZohoCoreService {
         return (await this.executeAPI('POST', moduleName, null, null, {
             data: data,
         })) as RecordApiResponse[];
+    }
+
+    // for Attachments and Profile Photos
+    async upload(
+        moduleName: string,
+        recordId: string,
+        extraOpts: ZohoCrmOptions,
+    ) {
+        if (!isDefined(extraOpts.fileType)) {
+            throw new BadRequestException(
+                BadRequestExceptionType.MISSING_FILE_TYPE,
+            );
+        }
+
+        if (!isDefined(extraOpts.file)) {
+            throw new BadRequestException(BadRequestExceptionType.MISSING_FILE);
+        }
+
+        const result = await this.executeAPI(
+            'POST',
+            moduleName,
+            recordId,
+            null,
+            null,
+            extraOpts,
+        );
+
+        switch (extraOpts.fileType) {
+            case FileType.ATTACHMENT: // https://www.zoho.com/crm/developer/docs/api/v2/upload-attachment.html
+                return result[0] as RecordApiResponse;
+            case FileType.PHOTO: // https://www.zoho.com/crm/developer/docs/api/v2/upload-image.html
+                return 'SUCCESS';
+        }
+    }
+
+    // DOCS: https://www.zoho.com/crm/developer/docs/api/v2/upload-files-to-zfs.html
+    async uploadToZFS(options: ZohoCrmOptions) {
+        if (!isDefined(options.file)) {
+            throw new BadRequestException(BadRequestExceptionType.MISSING_FILE);
+        }
+
+        const result = await this.executeAPI(
+            'POST',
+            'files',
+            null,
+            null,
+            null,
+            options,
+        );
+
+        return result as FilesApiResponse[];
+    }
+
+    // DOCS: https://www.zoho.com/crm/developer/docs/api/v2/get-attachments.html
+    async getAttachments(moduleName: string, recordId: string) {
+        return (await this.executeAPI('GET', moduleName, recordId, null, null, {
+            fileType: FileType.ATTACHMENT,
+        })) as RecordApiResponse[];
+    }
+
+    // DOCS: https://www.zoho.com/crm/developer/docs/api/v2/delete-attachments.html
+    async deleteAttachment(
+        moduleName: string,
+        recordId: string,
+        attachmentId: string,
+    ) {
+        const result: RecordApiResponse[] = await this.executeAPI(
+            'DELETE',
+            moduleName,
+            recordId,
+            null,
+            null,
+            {
+                fileType: FileType.ATTACHMENT,
+                fileId: attachmentId,
+            },
+        );
+
+        return result[0];
+    }
+
+    // DOCS: https://www.zoho.com/crm/developer/docs/api/v2/delete-image.html
+    async deletePhoto(moduleName: string, recordId: string) {
+        await this.executeAPI('DELETE', moduleName, recordId, null, null, {
+            fileType: FileType.PHOTO,
+        });
+
+        return 'SUCCESS';
+    }
+
+    async previewAttachment(
+        moduleName: string,
+        recordId: string,
+        attachmentId: string,
+    ) {
+        const result = await this.executeAPI(
+            'GET',
+            moduleName,
+            recordId,
+            null,
+            null,
+            {
+                fileType: FileType.ATTACHMENT,
+                fileId: attachmentId,
+                isDownload: true,
+            },
+        );
+
+        return result as {
+            data: any;
+            contentType: string;
+            contentDisposition: string;
+        };
+    }
+
+    async previewPhoto(moduleName: string, recordId: string) {
+        const result = await this.executeAPI(
+            'GET',
+            moduleName,
+            recordId,
+            null,
+            null,
+            {
+                fileType: FileType.PHOTO,
+                isDownload: true,
+            },
+        );
+
+        return result as {
+            data: any;
+            contentType: string;
+            contentDisposition: string;
+        };
     }
 
     async update(
@@ -68,9 +207,9 @@ export class ZohoCrmService extends ZohoCoreService {
     }
 
     async selectQuery(query: string) {
-        return this.executeAPI('POST', 'coql', null, null, {
+        return (await this.executeAPI('POST', 'coql', null, null, {
             select_query: query,
-        });
+        })) as CoqlApiResponse;
     }
 
     private async executeAPI(
@@ -92,9 +231,14 @@ export class ZohoCrmService extends ZohoCoreService {
 
         return lastValueFrom(observable)
             .then((response: AxiosResponse<any>) =>
-                this.responseHandler(response, moduleName, method),
+                this.responseHandler(
+                    response,
+                    moduleName,
+                    method,
+                    extraOpts.isDownload,
+                ),
             )
-            .catch((err) => this.errorHandler(err));
+            .catch((err) => this.errorHandler(err, moduleName, method));
     }
 
     private async executeAxiosInstance(
@@ -138,34 +282,44 @@ export class ZohoCrmService extends ZohoCoreService {
         return this.httpService.request(options);
     }
 
+    // noinspection JSMethodCanBeStatic
     private bodyHandler(extraOpts: ZohoCrmOptions, body: any, options: any) {
-        if (extraOpts.fileUpload && extraOpts.fileUpload.length > 0) {
-            const formDataObject: any = {
-                ...body,
-            };
+        if (extraOpts.isDownload) {
+            options['responseType'] = 'stream';
+        }
 
-            extraOpts.fileUpload.forEach((file: File) => {
-                if (!(file.fieldName in formDataObject)) {
-                    formDataObject[file.fieldName] = [];
-                }
-
-                formDataObject[file.fieldName].push({
-                    value: file.buffer,
-                    options: {
-                        filename: file.originalName,
-                        contentType: file.mimeType,
-                    },
-                });
-            });
+        if (extraOpts.file) {
+            const file: File = extraOpts.file;
 
             const formData = new FormData();
-            for (const formDataKey in formDataObject) {
-                formData.append(formDataKey, formDataObject[formDataKey]);
-            }
+
+            formData.append(file.fieldname, file.buffer, {
+                filename: file.originalname,
+                contentType: file.mimetype,
+            });
+
+            const formHeaders = formData.getHeaders();
 
             options['data'] = formData;
+            options['headers'] = {
+                ...options['headers'],
+                ...formHeaders,
+            };
         } else {
             options['data'] = body;
+        }
+
+        switch (extraOpts.fileType) {
+            case FileType.ATTACHMENT:
+                options['url'] = options['url'] + '/' + 'Attachments';
+                break;
+            case FileType.PHOTO:
+                options['url'] = options['url'] + '/' + 'photo';
+                break;
+        }
+
+        if (extraOpts.fileId) {
+            options['url'] = options['url'] + '/' + extraOpts.fileId;
         }
     }
 
@@ -173,10 +327,21 @@ export class ZohoCrmService extends ZohoCoreService {
         response: AxiosResponse<any>,
         moduleName: string,
         method: string,
+        isDownload: boolean,
     ) {
         // Zoho undefined dönderiyor aranan coql bulamayınca. Undefined sıkıntılarını çözmek için.
         if (!response) {
             return { data: [] };
+        }
+
+        if (isDownload) {
+            if (response.status === 204) throw new NotFoundException();
+
+            return {
+                data: response.data,
+                contentType: response.headers['content-type'],
+                contentDisposition: response.headers['content-disposition'],
+            };
         }
 
         const info = response.data.info;
