b0f3b3de401aa58ee34ad0d855d4a886f1b8dafc
Berkant BEGDILILI <59812010+berkantbegdilili@users.noreply.github.com>
2022-02-28 13:39:02 +0300

    ZohoCrmOptions interface have been updated.

--- src/modules/zoho/core/crm/interfaces/crm-options.interface.ts
+++ src/modules/zoho/core/crm/interfaces/crm-options.interface.ts
@@ -2,5 +2,13 @@ import { File } from '@modules/zoho/core/crm/interfaces/file.interface';
 
 export interface ZohoCrmOptions {
     isNotRequiredToken?: boolean;
-    fileUpload?: File[];
+    isDownload?: boolean;
+    file?: File;
+    fileId?: string;
+    fileType?: FileType;
+}
+
+export enum FileType {
+    PHOTO,
+    ATTACHMENT,
 }
